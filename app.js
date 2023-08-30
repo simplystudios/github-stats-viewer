5b35d8ca0eb1a9b300248730c5a59e9184033e91
simplystudios <new.anshwadhwa@gmail.com>
2023-08-30 19:53:00 +0530

    new

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const Button = document.getElementById('searchbutton')
 const Uiid = document.getElementById('search')
 
+async function apiget(api) {
+ const response = await fetch(api)
+ const data = await response.json();
+ document.getElementById('pfp').src = data.avatar_url
+ document.getElementById("nameu").textContent = data.name;
+ document.getElementById("desc").textContent = data.bio;
+ document.getElementById("create").textContent = `account created on : ${data.created_at}`;
+ document.getElementById("uname").textContent = `@${data.login}`;
+}
+
 const Search = () => {
          username = Uiid.value;
          console.log(username)
          if (username > ''){
+            apiget(`https://api.github.com/users/${username}`)
             document.getElementById("stat").src = `https://github-readme-stats.vercel.app/api?username=${username}&theme=react&show_icons=true&hide_border=true&count_private=true`;
             document.getElementById("streak").src = `https://streak-stats.demolab.com?user=${username}&theme=react&show_icons=true&hide_border=true&count_private=true`;
             document.getElementById("languse").src = `https://github-readme-stats.vercel.app/api/top-langs/?username=${username}&theme=react&show_icons=true&hide_border=true&layout=compact`;
-            document.getElementById("name").textContent = `Stats of ${username}`
+            document.getElementById("name").textContent = `Stats for ${username}`
          }
         else{
             document.getElementById("name").textContent = 'Type A Username ex : simplystudios'
