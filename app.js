4c4fe638980ac8abd70eabba93de329c7af21b97
simplystudios <new.anshwadhwa@gmail.com>
2023-10-03 22:01:51 +0530

    new

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const Uiid = document.getElementById('search')
 async function apiget(api) {
  const response = await fetch(api)
  const data = await response.json();
+ const createdate = data.created_at
+ const datetime = createdate.replace('T','  ')
  document.getElementById('pfp').src = data.avatar_url
  document.getElementById("nameu").textContent = data.name;
  document.getElementById("desc").textContent = data.bio;
- document.getElementById("create").textContent = `account created on : ${data.created_at}`;
+ document.getElementById("create").textContent = `account created on : ${datetime}`;
  document.getElementById("uname").textContent = `@${data.login}`;
 }
 
@@ -25,5 +27,10 @@ const Search = () => {
             document.getElementById("name").textContent = 'Type A Username ex : simplystudios'
         }
 }
-
 Button.addEventListener("click",Search())
+search.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        Search()
+        // Perform desired actions here
+      }
+    });
